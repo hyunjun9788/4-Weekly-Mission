6edec39e7a9aa70a728128619079f5d1941e34ed
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-28 18:21:14 +0900

    로그인, 회원가입 Input 컴포넌트 생성

--- week7/src/EmptyPage.tsx
+++ week7/src/EmptyPage.tsx
@@ -1,5 +1,13 @@
+import Input from "./components/Input/Input.tsx";
+
 function EmptyPage() {
-  return <h1>시작페이지</h1>;
+  return (
+    <>
+      <h1>시작페이지</h1>
+      <Input isEmailOrPw="email" />
+      <Input isEmailOrPw="password" />
+    </>
+  );
 }
 
 export default EmptyPage;
--- week7/src/custom.d.ts
+++ week7/src/custom.d.ts
@@ -3,3 +3,7 @@ declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.svg";
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export = content;
+}
