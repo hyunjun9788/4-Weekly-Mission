4c7e2eebae04cadbfec0add820d3cc378a3ae529
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-08 03:02:31 +0900

    Refactor: Title 컴포넌트 이동

---
+++ weekly-mission/src/components/common/Title.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import logo from "../../../public/images/Linkbrary.png";
+import Link from "next/link";
+import styles from "./Title.module.css";
+
+interface TitleProps {
+  text: string;
+  btn: string;
+}
+export const Title = (props: TitleProps) => {
+  return (
+    <div className={styles.title}>
+      <Image width={210} height={38} src={logo} alt="linkbrary" />
+      <div>
+        <span>{props.text} </span>
+        <Link
+          className={styles.signupBtn}
+          href={props.btn === "회원 가입하기" ? "/signup" : "/signin"}
+        >
+          {props.btn}
+        </Link>
+      </div>
+    </div>
+  );
+};
